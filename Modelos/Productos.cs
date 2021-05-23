f28c94250f7e3896f7fa708bb74ea2a3223d1241
CamachoSarmiento <CamachoSarmiento@CARLOSCAMACHO>
2021-05-23 16:18:41 -0500

    Test de Facturacion

---
+++ Modelos/Productos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelos
+{
+    class Productos
+    {
+        /// <summary>
+        /// Nombre del Producto
+        /// </summary>
+        public string NombreProducto { get; set; }
+        /// <summary>
+        /// Existencias de Inventario del producto
+        /// </summary>
+        public int Existencias { get; set; }
+        /// <summary>
+        /// Valor total del producto
+        /// </summary>
+        public int Valor { get; set; }
+        /// <summary>
+        /// Valor de Iva del producto
+        /// </summary>
+        public int ValorIva { get; set; }
+        /// <summary>
+        /// Producto Habilidado
+        /// </summary>
+        public bool Habilitado { get; set; }
+        /// <summary>
+        /// Categoria del producto
+        /// </summary>
+        public int IdCategoria { get; set; }
+    }
+}
---
+++ Servicios/ProductosRepository.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Servicios
+{
+    class ProductosRepository
+    {
+        private readonly string _connectionString;
+
+        public ProductosRepository(IConfiguration configuration)
+        {            
+            _connectionString = configuration.GetConnectionString("FacturacionConnection");
+        }
+    }
+}
