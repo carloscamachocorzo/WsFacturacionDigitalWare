d5d4bb097cc83534917ae75c2d8d8309d0699acf
CarlosCamachoCorzo <CarlosCamachoCorzo@CARLOSCAMACHO>
2021-06-10 17:46:54 -0500

    Creacion de DTO de Facturas

---
+++ Modelos/Dtos/FacturasDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modelos.Dtos
+{
+    class FacturasDto
+    {
+        public decimal NumeroFactura { get; set; }
+        public string PrefijoFactura { get; set; }
+        public string Estado { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
